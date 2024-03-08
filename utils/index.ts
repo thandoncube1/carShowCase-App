9a34e06a82e23e82d19f665a340bfa8a09f8c642
thandon263 <ncubethando20@gmail.com>
2024-03-08 18:27:01 -0500

    Adding the env file in the list

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { CarProps, FilterProps } from "@/types";
 export async function fetchCars(filters: FilterProps) {
     const { manufacturer, model, year, limit, fuel } = filters;
     const headers = {
-        'X-RapidAPI-Key': '7a21771f1bmsh661d5512455f38fp1a7ba4jsnbba2014063af',
+        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
