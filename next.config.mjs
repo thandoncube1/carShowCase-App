c53ba6adbe763c663192cd1a37e929c79b013da5
thandon263 <ncubethando20@gmail.com>
2024-03-09 07:39:16 -0500

    Disabling eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdn.imagin.studio']
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    }
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
